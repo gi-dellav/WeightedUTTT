05c011fb2455f2b178b8a989b63dcba389d9a004
Your Name <djtech.developer@gmail.com>
2025-10-19 00:30:59 +0200

    refactor: change weighted parameters from f32 to i32

--- src/weighted.rs
+++ src/weighted.rs
@@ -1,24 +1,26 @@
+use cached::proc_macro::cached;
+
 use crate::defs::{Cell, Coord, Grid, Player};
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
 pub struct WeightedParameters {
-    pub take_cell: f32,          // 0~1.0
-    pub take_double_cell: f32,   // 0~1.0
-    pub take_double_grid: f32,   // 0~1.0
-    pub stop_cell: f32,          // 0~1.0
-    pub stop_win: f32,           // 0~1.0
-    pub stop_double_cell: f32,   // 0~1.0
-    pub stop_double_grid: f32,   // 0~1.0
-    pub giving_cell: f32,        // -1.0~1.0
-    pub giving_double_cell: f32, // -1.0~1.0
-    pub giving_double_grid: f32, // -1.0~1.0
-    pub play_corner: f32,        // -1.0~1.0
-    pub play_sides: f32,         // -1.0~1.0
-    pub play_center: f32,        // -1.0~1.0
-    pub best_enemy_move: f32,    // -1.0~1.0
+    pub take_cell: i32,          // 0~1.0    (*10**9 for integer rappresentation)
+    pub take_double_cell: i32,   // 0~1.0
+    pub take_double_grid: i32,   // 0~1.0
+    pub stop_cell: i32,          // 0~1.0
+    pub stop_win: i32,           // 0~1.0
+    pub stop_double_cell: i32,   // 0~1.0
+    pub stop_double_grid: i32,   // 0~1.0
+    pub giving_cell: i32,        // -1.0~1.0
+    pub giving_double_cell: i32, // -1.0~1.0
+    pub giving_double_grid: i32, // -1.0~1.0
+    pub play_corner: i32,        // -1.0~1.0
+    pub play_sides: i32,         // -1.0~1.0
+    pub play_center: i32,        // -1.0~1.0
+    pub best_enemy_move: i32,    // -1.0~1.0
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
 pub struct WeightedPlayer {
     pub weighted_params: WeightedParameters,
     pub apply_softsign: bool,
@@ -36,6 +38,11 @@ impl WeightedPlayer {
             ignore_giving: false,
         }
     }
+}
+
+#[cached]
+fn eval_board(params: WeightedParameters, grid: Grid) -> f32 {
+    todo!("Implement eval_board");
 }
 
 impl Player for WeightedPlayer {
